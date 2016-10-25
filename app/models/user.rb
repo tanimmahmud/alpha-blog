1e7b7c4be3f351a28afd317f499dfa3179c993e7
Tanim Mahmud <tanimmahmud08@gmail.com>
2016-10-26 08:33:48 +1100

    Add sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,5 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false},
             length: { maximum: 105},
             format: { with: VALID_EMAIL_REGEX}
-  has_secure_password
+ has_secure_password
 end
